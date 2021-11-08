8869038f66c94713c711973f39657102405490d2
Junior042 <eduardoalvesdeoliveirajunior@gmail.com>
2021-11-08 17:47:25 -0300

    fix refatoração de codigo

--- main.js
+++ main.js
@@ -1,48 +1,33 @@
 // Display e Resultado
-let value = null;
+var value = null;
 let arr = [];
 
-// Numeros 
-let btn_0 = document.querySelector('#btn_0').value;
-let btn_1 = document.querySelector('#btn_1').value;
-let btn_2 = document.querySelector('#btn_2').value;
-let btn_3 = document.querySelector('#btn_3').value;
-let btn_4 = document.querySelector('#btn_4').value;
-let btn_5 = document.querySelector('#btn_5').value;
-let btn_6 = document.querySelector('#btn_6').value;
-let btn_7 = document.querySelector('#btn_7').value;
-let btn_8 = document.querySelector('#btn_8').value;
-let btn_9 = document.querySelector('#btn_9').value;
-
-// sinais 
-let btn_menos = document.querySelector('#btn_menos').value;
-let btn_mais = document.querySelector('#btn_mais').value;
-let btn_multiplica = document.querySelector('#btn_multiplica').value;
-let btn_divide = document.querySelector('#btn_divide').value;
-let btn_igual = document.querySelector('#btn_igual').value;
-
-
+// Função pegaValor 
 function pegaValor(dado){
   let display = document.querySelector('#display');
   display.value = dado;
 
   if(display.value != undefined || display.value != null){
-  let value = display.value;
+  value = display.value;
   arr.push(display.value);
   display.innerText = arr.join('');
   }
 }
 
+// Função resultado 
 function res(){
   display.innerText = eval(arr.join(''));
   arr = [];
 }
 
+
+// Função de Resetar  
 function reset(){
   arr = [];
   display.innerText = 'Foi Resetado!';
 }
 
+// Função de Deletar 
 function del(){
   arr.pop()
   display.innerText = arr.join('');
